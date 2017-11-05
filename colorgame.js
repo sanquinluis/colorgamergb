51266cf141d375eae026fc5cfaf2bc501566d20f
Hector L. Santana-Quintana <luisantanamusic@icloud.com>
2017-11-04 20:27:55 -0400

    Selecting the correct rgb works

---
+++ colorgame.js
@@ -0,0 +1,48 @@
+// array of basic colors set to default when the game starts
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255,255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
+	]
+	// selecting with querySelectorAll the class square to loop and set up the colors.
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+	
+	for(var i = 0;i < squares.length; i++){
+		//add initial colors to squares
+		squares[i].style.backgroundColor = colors[i]
+
+		squares[i].addEventListener("click", function(){
+			//grab color of clicked square
+			var clickedColor = this.style.backgroundColor;
+			// compare color to pickedColor
+				if(clickedColor === pickedColor){
+					messageDisplay.textContent = "Correct!!";
+					changeColors(clickedColor);
+				}else {
+					this.style.backgroundColor = "#232323";
+					messageDisplay.textContent = "Try Again";
+				}
+		});
+			
+	}
+	//function that loops throgh the array of colors and change them to the same color
+function changeColors(color){
+		//loop through all squares
+		for(var i = 0; i < squares.length; i++){
+			//change each color to match given color
+			squares[i].style.backgroundColor = color;
+		}
+	}
+
+function pickColor() {
+		var random = Math.floor(Math.random() * colors.length);
+		return colors[random];
+}
