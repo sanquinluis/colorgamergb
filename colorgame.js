978b0cb89e64f3ece79fa7c82421d1c0203c4e5b
Hector L. Santana-Quintana <luisantanamusic@icloud.com>
2017-11-08 22:01:17 -0500

    Dificult and easy mode

--- colorgame.js
+++ colorgame.js
@@ -1,5 +1,6 @@
 // array of basic colors set to default when the game starts
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 // hard coded array
 // [
 // 	"rgb(255, 0, 0)",
@@ -16,7 +17,43 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
+// changing the easy and hard mode
+easyBtn.addEventListener("click", function(){
+	// alert("hey Easy");
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+hardBtn.addEventListener("click", function(){
+	// alert("Hey Hard");
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+	}
+});
+
+
+
+// Reset the game function
 	resetButton.addEventListener("click", function(){
 	// generate all new colors
 		colors = generateRandomColors(6);
